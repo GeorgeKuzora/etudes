0f0ebe2d1d42fc8f70aa18d2c1c9c2959a4b4412
GeorgiyKuzora <g.kuzora@korona.net>
2026-07-26 20:25:17 +0300

    aoc2025-rust day4

--- aoc2025-rust/day4/src/main.rs
+++ aoc2025-rust/day4/src/main.rs
@@ -69,9 +69,19 @@ impl<'a> Grid<'a> {
         && (address.y as usize) < self.height
     }
 
+    fn remove(&mut self, address: &Coordinates) {
+        if !self.in_bounds(address) {
+            return ();
+        }
+        let id = self.width * address.y as usize + address.x as usize;
+        self.data[id] = &EMPTY_SPACE;
+    }
+
 }
 
 fn main() {
+    // PART 1
+
     let input = read_to_string("input.txt").expect("file should be in path");
 
     let width = input.lines().next().map_or(0, str::len);
@@ -101,5 +111,84 @@ fn main() {
         }
     }).count();
 
-    println!("Count for accessible rolls without moving: {accessible_count}")
+    println!("Count for accessible rolls without moving: {accessible_count}");
+
+    // PART 2
+
+    let input = read_to_string("input.txt").expect("file should be in path");
+
+    let width = input.lines().next().map_or(0, str::len);
+    let height = input.lines().count();
+
+    let data: Vec<&u8> = input.lines().flat_map(str::as_bytes).collect();
+
+    let mut grid = Grid::new(data, width, height);
+
+    let coordinates: Vec<Coordinates> = (0..height).flat_map(|y| {
+        (0..width).map(move |x| Coordinates{ x: x as isize, y: y as isize })
+    }).collect();
+
+    // VARIANT 1 WITH FILTER COUNT
+
+    // let mut accessible_count = 0;
+
+    // loop {
+    //     let current_count = coordinates.iter().filter(|address| {
+    //         match grid.get(address) {
+    //             Some(item) => {
+    //                 if item == &ROLL_OF_PAPER {
+    //                     match grid.is_accessible(address) {
+    //                         Some(is_accessible) => {
+    //                             if is_accessible {
+    //                                 grid.remove(address);
+    //                             }
+    //                             is_accessible
+    //                         },
+    //                         None => false,
+    //                     }
+    //                 } else {
+    //                     false
+    //                 }
+    //             },
+    //             None => false,
+    //         }
+    //     }).count();
+    //     if current_count == 0 {
+    //         break;
+    //     }
+    //     accessible_count += current_count;
+    // }
+    // println!("Count for accessible rolls with moving: {accessible_count}");
+
+    // VARIANT 2 WITH FOR LOOP
+
+    let mut accessible_count = 0;
+
+    loop {
+        let mut had_accessible = false;
+        for address in coordinates.iter() {
+            match grid.get(address) {
+                Some(item) => {
+                    if item == &ROLL_OF_PAPER {
+                        match grid.is_accessible(address) {
+                            Some(is_accessible) => {
+                                if is_accessible {
+                                    accessible_count += 1;
+                                    had_accessible = true;
+                                    grid.remove(address);
+                                }
+                            },
+                            None => (),
+                        }
+                    } else {
+                        ()
+                    }
+                },
+                None => (),
+            }
+        }
+        if !had_accessible { break }
+    }
+
+    println!("Count for accessible rolls with moving: {accessible_count}");
 }
